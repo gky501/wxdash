885df870fb342a1fbc85f5c3a03f6fc68196a9a9
gky501 <aaron@aarongilkey.net>
2026-04-30 07:29:50 -0500

    Update MEMS Weather Center database with new data
    Added weather data for various areas, including stations, locations, and towns.

--- assets/data/weather-database.js
+++ assets/data/weather-database.js
@@ -1,1 +1,440 @@
 
+// MEMS Weather Center database
+// Update stations, locations, towns, and area buttons here.
+window.MEMS_WEATHER_DATABASE = {
+  "areaButtons": [
+    {
+      "id": "metro",
+      "label": "METRO",
+      "title": "METRO",
+      "center": [
+        34.74,
+        -92.3
+      ],
+      "stations": [
+        "Station 1",
+        "Station 3",
+        "Station 4",
+        "Station 10",
+        "MEMS North"
+      ]
+    },
+    {
+      "id": "maumelle",
+      "label": "MAUMELLE",
+      "title": "MAUMELLE",
+      "center": [
+        34.86,
+        -92.4
+      ],
+      "stations": [
+        "Maumelle Station"
+      ]
+    },
+    {
+      "id": "north-pulaski",
+      "label": "NORTH PULASKI",
+      "title": "NORTH PULASKI",
+      "center": [
+        34.9,
+        -92.17
+      ],
+      "stations": [
+        "North Pulaski Station",
+        "Gravel Ridge Station"
+      ]
+    },
+    {
+      "id": "arch-street",
+      "label": "ARCH ST",
+      "title": "ARCH STREET",
+      "center": [
+        34.62,
+        -92.31
+      ],
+      "stations": [
+        "Arch St Station"
+      ]
+    },
+    {
+      "id": "lonoke",
+      "label": "LONOKE CO",
+      "title": "LONOKE COUNTY",
+      "center": [
+        34.97,
+        -92.01
+      ],
+      "stations": [
+        "Cabot Station"
+      ]
+    },
+    {
+      "id": "grant",
+      "label": "GRANT COUNTY",
+      "title": "GRANT COUNTY",
+      "center": [
+        34.31,
+        -92.39
+      ],
+      "stations": [
+        "Sheridan Station",
+        "Sheridan Fire Station"
+      ]
+    }
+  ],
+  "stations": [
+    {
+      "name": "Station 1",
+      "area": "Little Rock",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7433819,
+      "lng": -92.2843613
+    },
+    {
+      "name": "Station 3",
+      "area": "Little Rock",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.669039,
+      "lng": -92.3581024
+    },
+    {
+      "name": "Station 4",
+      "area": "Little Rock / NLR",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7593041,
+      "lng": -92.3802302
+    },
+    {
+      "name": "Station 10",
+      "area": "Little Rock",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7991817,
+      "lng": -92.4554265
+    },
+    {
+      "name": "MEMS North",
+      "area": "Little Rock / NLR",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7812027,
+      "lng": -92.2743301
+    },
+    {
+      "name": "Maumelle Station",
+      "area": "Maumelle",
+      "zone": [
+        "maumelle"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.8569051,
+      "lng": -92.4070432
+    },
+    {
+      "name": "North Pulaski Station",
+      "area": "North Pulaski / Sherwood",
+      "zone": [
+        "north-pulaski"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.9403325,
+      "lng": -92.1683398
+    },
+    {
+      "name": "Gravel Ridge Station",
+      "area": "NLR / Sherwood",
+      "zone": [
+        "north-pulaski"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.8686392,
+      "lng": -92.1978871
+    },
+    {
+      "name": "Arch St Station",
+      "area": "South Pulaski / Grant County",
+      "zone": [
+        "arch-street"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.6174833,
+      "lng": -92.3157304
+    },
+    {
+      "name": "Cabot Station",
+      "area": "Cabot / Lonoke County",
+      "zone": [
+        "lonoke"
+      ],
+      "countyCodes": [
+        "ARC085"
+      ],
+      "lat": 34.9732158,
+      "lng": -92.0171126
+    },
+    {
+      "name": "Sheridan Station",
+      "area": "Sheridan / Grant County",
+      "zone": [
+        "grant"
+      ],
+      "countyCodes": [
+        "ARC053"
+      ],
+      "lat": 34.3170329,
+      "lng": -92.3955182
+    },
+    {
+      "name": "Sheridan Fire Station",
+      "area": "Sheridan / Grant County",
+      "zone": [
+        "grant"
+      ],
+      "countyCodes": [
+        "ARC053"
+      ],
+      "lat": 34.3073,
+      "lng": -92.3963
+    }
+  ],
+  "locations": [
+    {
+      "name": "Greystone",
+      "area": "Cabot",
+      "type": "Neighborhood",
+      "lat": 35.02923764117509,
+      "lng": -92.05865556970093,
+      "radius": "2 miles"
+    },
+    {
+      "name": "Clinton Nat. Airport",
+      "area": "Little Rock",
+      "type": "Airport",
+      "lat": 34.7284011058131,
+      "lng": -92.22039216234353,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Wrightsville Correctional Unit",
+      "area": "Wrightsville",
+      "type": "Correctional",
+      "lat": 34.60194061960711,
+      "lng": -92.19260013773142,
+      "radius": ".25 miles"
+    },
+    {
+      "name": "LR Air Force Base",
+      "area": "Jacksonville",
+      "type": "Military",
+      "lat": 34.90639838445616,
+      "lng": -92.14395233018587,
+      "radius": "1 mile"
+    },
+    {
+      "name": "LR Veterans Hospital",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.746267149080055,
+      "lng": -92.32047667930003,
+      "radius": "1 mile"
+    },
+    {
+      "name": "UAMS",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.74744666257331,
+      "lng": -92.32088969840527,
+      "radius": ".25 miles"
+    },
+    {
+      "name": "St. Vincent Infirmary",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.75033284961561,
+      "lng": -92.33940377918991,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Baptist Med Center LR",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.74480435362505,
+      "lng": -92.38089211015347,
+      "radius": ".25 miles"
+    },
+    {
+      "name": "Arkansas Heart Hospital",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.73922859302091,
+      "lng": -92.39358428834757,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Arkansas Childrens",
+      "area": "Little Rock",
+      "type": "Hospital",
+      "lat": 34.74198874170889,
+      "lng": -92.29300715213085,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Saline Memorial",
+      "area": "Benton",
+      "type": "Hospital",
+      "lat": 34.57494971990144,
+      "lng": -92.59375497712537,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Baptist Springhill",
+      "area": "North Little Rock",
+      "type": "Hospital",
+      "lat": 34.78394874277726,
+      "lng": -92.22312924338553,
+      "radius": "1 mile"
+    },
+    {
+      "name": "SVI North",
+      "area": "Sherwood",
+      "type": "Hospital",
+      "lat": 34.81344283040206,
+      "lng": -92.20907267741393,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Unity Jacksonville",
+      "area": "Jacksonville",
+      "type": "Hospital",
+      "lat": 34.87316535170868,
+      "lng": -92.12534698787888,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Cabot Emergency Hospital",
+      "area": "Cabot",
+      "type": "Hospital",
+      "lat": 34.98186175242931,
+      "lng": -92.03483579161522,
+      "radius": "1 mile"
+    },
+    {
+      "name": "Pinnacle MTN",
+      "area": "Little Rock",
+      "type": "Landmark",
+      "lat": 34.84164634982719,
+      "lng": -92.48591891234585,
+      "radius": ".5 miles"
+    }
+  ],
+  "towns": [
+    {
+      "name": "Little Rock",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7465,
+      "lng": -92.2896
+    },
+    {
+      "name": "North Little Rock",
+      "zone": [
+        "metro"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.7695,
+      "lng": -92.2671
+    },
+    {
+      "name": "Maumelle",
+      "zone": [
+        "maumelle"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.8668,
+      "lng": -92.4043
+    },
+    {
+      "name": "Sherwood",
+      "zone": [
+        "north-pulaski"
+      ],
+      "countyCodes": [
+        "ARC119"
+      ],
+      "lat": 34.8151,
+      "lng": -92.2243
+    },
+    {
+      "name": "Cabot",
+      "zone": [
+        "lonoke"
+      ],
+      "countyCodes": [
+        "ARC085"
+      ],
+      "lat": 34.9745,
+      "lng": -92.0165
+    },
+    {
+      "name": "Lonoke",
+      "zone": [
+        "lonoke"
+      ],
+      "countyCodes": [
+        "ARC085"
+      ],
+      "lat": 34.7839,
+      "lng": -91.8999
+    },
+    {
+      "name": "Sheridan",
+      "zone": [
+        "grant"
+      ],
+      "countyCodes": [
+        "ARC053"
+      ],
+      "lat": 34.307,
+      "lng": -92.4013
+    }
+  ]
+};
